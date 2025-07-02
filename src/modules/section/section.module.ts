24997703f63dfed726b93daef4c4f036d505bfc3
Besher-Hamze <besherhamze97@gmail.com>
2025-07-02 13:54:56 +0300

    e

--- src/modules/section/section.module.ts
+++ src/modules/section/section.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SectionService } from './section.service';
 import { SectionController } from './section.controller';
 import { Section, SectionSchema } from './schemas/section.schema';
+import { Task, TaskSchema } from '../task/schema/task.schema';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Section.name, schema: SectionSchema }])],
+    imports: [MongooseModule.forFeature([{ name: Section.name, schema: SectionSchema }, { name: Task.name, schema: TaskSchema }])],
     controllers: [SectionController],
     providers: [SectionService],
     exports: [SectionService],
